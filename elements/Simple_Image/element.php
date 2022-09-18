dc75bef3cc4df4e36340708fb4058fcb541b3315
alpabuz <alpabuz@mail.ru>
2022-09-18 19:59:31 +0500

    Element created
    The element is based on the base Image element.
    
    - Simplified structure and settings
    - Added Aspect Ratio setting

---
+++ elements/Simple_Image/element.php
@@ -0,0 +1,253 @@
+<?php
+
+namespace EssentialElements;
+
+use function Breakdance\Elements\c;
+use function Breakdance\Elements\PresetSections\getPresetSection;
+
+
+\Breakdance\ElementStudio\registerElementForEditing(
+    "EssentialElements\\SimpleImage",
+    \Breakdance\Util\getdirectoryPathRelativeToPluginFolder(__DIR__)
+);
+
+class SimpleImage extends \Breakdance\Elements\Element
+{
+    static function uiIcon()
+    {
+        return 'ImageIcon';
+    }
+
+    static function tag()
+    {
+        return 'div';
+    }
+
+    static function tagOptions()
+    {
+        return [];
+    }
+
+    static function tagControlPath()
+    {
+        return false;
+    }
+
+    static function name()
+    {
+        return 'Simple Image';
+    }
+
+    static function className()
+    {
+        return 'oxd-simple-image';
+    }
+
+    static function category()
+    {
+        return 'basic';
+    }
+
+    static function badge()
+    {
+        return false;
+    }
+
+    static function slug()
+    {
+        return get_class();
+    }
+
+    static function template()
+    {
+        return file_get_contents(__DIR__ . '/html.twig');
+    }
+
+    static function defaultCss()
+    {
+        return file_get_contents(__DIR__ . '/default.css');
+    }
+
+    static function defaultProperties()
+    {
+        return false;
+    }
+
+    static function defaultChildren()
+    {
+        return false;
+    }
+
+    static function cssTemplate()
+    {
+        $template = file_get_contents(__DIR__ . '/css.twig');
+        return $template;
+    }
+
+    static function designControls()
+    {
+        return [getPresetSection(
+      "EssentialElements\\borders",
+      "Borders",
+      "borders",
+       ['type' => 'popout']
+     ), getPresetSection(
+      "EssentialElements\\spacing_margin_y",
+      "Spacing",
+      "spacing",
+       ['type' => 'popout']
+     )];
+    }
+
+    static function contentControls()
+    {
+        return [c(
+        "content",
+        "Content",
+        [c(
+        "image",
+        "Image",
+        [],
+        ['type' => 'wpmedia', 'layout' => 'vertical', 'mediaOptions' => ['acceptedFileTypes' => ['0' => 'image'], 'multiple' => false]],
+        false,
+        false,
+        [],
+      ), c(
+        "size",
+        "Image Size",
+        [],
+        ['type' => 'media_size_dropdown', 'layout' => 'vertical', 'mediaSizeOptions' => ['imagePropertyPath' => 'content.content.image', 'disableSrcset' => false]],
+        false,
+        false,
+        [],
+      ), c(
+        "width",
+        "Width",
+        [],
+        ['type' => 'unit', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      ), c(
+        "aspect_ratio",
+        "Aspect Ratio",
+        [],
+        ['type' => 'text', 'layout' => 'inline', 'placeholder' => '4/3'],
+        false,
+        false,
+        [],
+      ), c(
+        "alt",
+        "Alt",
+        [c(
+        "alt",
+        "Alt",
+        [],
+        ['type' => 'button_bar', 'layout' => 'vertical', 'items' => ['0' => ['value' => 'disable', 'text' => 'Disable'], '1' => ['text' => 'Custom', 'value' => 'custom']]],
+        false,
+        false,
+        [],
+      ), c(
+        "custom_alt",
+        "Custom Alt",
+        [],
+        ['type' => 'text', 'layout' => 'vertical', 'condition' => ['path' => 'content.content.alt.alt', 'operand' => 'equals', 'value' => 'custom']],
+        false,
+        false,
+        [],
+      )],
+        ['type' => 'section', 'layout' => 'vertical', 'sectionOptions' => ['type' => 'popout']],
+        false,
+        false,
+        [],
+      ), c(
+        "lazy_load",
+        "Lazy Load",
+        [],
+        ['type' => 'toggle', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      )],
+        ['type' => 'section', 'layout' => 'vertical'],
+        false,
+        false,
+        [],
+      )];
+    }
+
+    static function settingsControls()
+    {
+        return [];
+    }
+
+    static function dependencies()
+    {
+        return false;
+    }
+
+    static function settings()
+    {
+        return false;
+    }
+
+    static function addPanelRules()
+    {
+        return ['alwaysHide' => false];
+    }
+
+    static public function actions()
+    {
+        return false;
+    }
+
+    static function nestingRule()
+    {
+        return ["type" => "final",   ];
+    }
+
+    static function spacingBars()
+    {
+        return ['0' => ['location' => 'outside-bottom', 'cssProperty' => 'margin-bottom', 'affectedPropertyPath' => 'design.spacing.margin_bottom.%%BREAKPOINT%%'], '1' => ['location' => 'outside-top', 'cssProperty' => 'margin-top', 'affectedPropertyPath' => 'design.spacing.margin_top.%%BREAKPOINT%%']];
+    }
+
+    static function attributes()
+    {
+        return false;
+    }
+
+    static function experimental()
+    {
+        return false;
+    }
+
+    static function order()
+    {
+        return 90;
+    }
+
+    static function dynamicPropertyPaths()
+    {
+        return ['0' => ['path' => 'content.content.image', 'accepts' => 'image_url'], '1' => ['accepts' => 'string', 'path' => 'content.content.alt.custom_alt']];
+    }
+
+    static function additionalClasses()
+    {
+        return false;
+    }
+
+    static function projectManagement()
+    {
+        return false;
+    }
+
+    static function propertyPathsToWhitelistInFlatProps()
+    {
+        return false;
+    }
+
+    static function propertyPathsToSsrElementWhenValueChanges()
+    {
+        return false;
+    }
+}
